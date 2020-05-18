48e2c756228238020baea746ccf98906c337bae4
Conor Dockry <conordockry@gmail.com>
2020-05-17 23:02:17 -0400

    fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,10 @@ export default function StaticDevApp({ Component, pageProps }) {
       <Head>
         <title>Static Dev</title>
         <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Playfair+Display:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet"
         ></link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.{jsx,js,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        serif: ["'Playfair Display'", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/ui')],
